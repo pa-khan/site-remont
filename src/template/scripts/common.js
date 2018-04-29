0d66984a499d9bd5db2134a6eaf88eae92a7787e
e5ash <e5ash.bro@gmail.com>
2018-04-29 16:31:50 +0300

    98%

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -1,5 +1,59 @@
 $(document).ready(function($) {
 
-	$('.text_phone').mask('+7 (999) 999-99-99');
-	
+	$('.input_tel input').mask('+7 (999) 999-99-99');
+
+	$('.types__list').slick({
+		prevArrow: '<button type="button" class="slick-prev slick-arrow"><i class="icon icon-arrow-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next slick-arrow"><i class="icon icon-arrow-right"></i></button>',
+	})
+
+
+	var worksImg = $('.works__main'),
+        worksImgs = $('.works__list');
+	worksImg.slick({
+		arrows: false,
+		asNavFor: worksImgs
+	})
+    worksImgs.slick({
+		slidesToShow: 5,
+        asNavFor: worksImg,
+		vertical: true,
+        responsive: [{
+			breakpoint: 1200,
+			settings: {
+                vertical: false
+            }}, {
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 4,
+                vertical: false,
+				arrows: false
+            }}, {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                vertical: false,
+                arrows: false
+            }
+        }]
+    })
+
+	$('.reviews__list').slick({
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		arrows: false,
+		dots: true,
+        responsive: [{
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                arrows: false
+            }
+        }]
+ 	})
+
+	$('.mobile-btn').click(function () {
+		$('.panel__nav').toggleClass('panel__nav_toggle');
+		$(this).toggleClass('mobile-btn_toggle');
+    })
 });
